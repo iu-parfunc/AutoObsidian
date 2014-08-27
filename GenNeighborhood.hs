b2a1157b423e000f81d55e78e28b8f86488acec6
Michael Vollmer <vollmerm@indiana.edu>
2014-08-26 21:39:28 -0400

    simple neighborhood generation for MidIR

---
+++ GenNeighborhood.hs
@@ -0,0 +1,57 @@
+
+-- IR datatype from last meeting
+
+type MidIR = [([Var], AExp)]
+data AExp  = V Var
+           | Map Exp Var | Fold Exp Var
+           | Split Var   | Concat Var Var
+           deriving (Show)
+
+type Var = Integer
+data Exp = Exp
+         deriving (Show)
+
+f :: Exp
+f = undefined
+
+g :: Exp
+g = undefined
+
+
+-- one example program and a permutation
+
+prog1 :: MidIR
+prog1 = [([1], Fold g 0),
+         ([2], Map f 1)]
+
+prog2 :: MidIR
+prog2 = [([1], Fold g 0),
+         ([3,2], Split 1),
+         ([5], (Map f 2)),
+         ([4], (Map f 3)),
+         ([6],  Concat 5 4)]
+
+
+
+-- generating some neighbors
+
+nextNum = (+1) . maximum . map (maximum . nums)
+  where nums (l, Fold _ v)   = v : l
+        nums (l, Split v)    = v : l
+        nums (l, Map _ v)    = v : l
+        nums (l, Concat v n) = n : v : l
+
+walkCode _   []   = []
+walkCode gen code = code'
+  where code' = case elem of
+          Just x -> x : tail code
+          Nothing -> head code : walkCode gen (tail code)
+        elem  = gen $ head code
+
+splits code = walkCode gen code
+  where num = nextNum code
+        gen (n, Map f n1)  = Just (n, Map f n1)
+        gen (n, Fold f n1) = Just (n, Fold f n1)
+        gen _               = Nothing
+        
+
