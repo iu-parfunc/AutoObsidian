d3a9739fddce8d7e2ad436e35d931a9f78f811ed
Joel Svensson <svenssonjoel@yahoo.se>
2015-04-07 11:30:03 -0400

    update

--- Applications/Mandel/FractalsMain.hs
+++ Applications/Mandel/FractalsMain.hs
@@ -15,13 +15,9 @@ import Control.Monad.State
 
 import Data.Int
 
-import Data.ByteString as BS hiding (putStrLn, length)
-
 import System.Environment
-import System.CPUTime.Rdtsc
 import System.Exit
 
-import Data.Time.Clock
 import Control.DeepSeq
 
 -- Autotuning framework 
@@ -30,9 +26,11 @@ import Auto.Score
 -- timing
 import Criterion.Main
 import Criterion.Types 
-
+import Criterion.Monad
+import Criterion.Internal
 
 threads = 256
+blocks  = 64
 image_size = 1024
 identity = 256
 
@@ -48,42 +46,25 @@ main = do
           threads
           (mandel image_size)
   
-  
-  
-  
-  withCUDA' ctx $
-    do    
-      transfer_start <- lift getCurrentTime
-      allocaVector (fromIntegral (s*s)) $ \o -> 
-        do
-          transfer_done <- lift getCurrentTime 
-          
-          t0   <- lift getCurrentTime
-          cnt0 <- lift rdtsc
-          forM_ [0..999] $ \_ -> do
-            o <== (s,kern)
+ 
+  let runIt = withCUDA' ctx $
+        allocaVector (fromIntegral (image_size*image_size)) $ \o -> 
+          do
+            -- threads here means blocks.
+            o <== (blocks,kern)
             syncAll
-          cnt1 <- lift rdtsc
-          t1   <- lift getCurrentTime
-          
-          r <- copyOut o 
-          t_end <- lift getCurrentTime
-  
-          -- Still going via list !!! 
-          lift $ BS.writeFile "fractal.out" (pack (V.toList r))
+            -- copyOut o 
 
-          lift $ putStrLn $ "SELFTIMED: " ++ show (diffUTCTime t1 t0)
-          lift $ putStrLn $ "CYCLES: "    ++ show (cnt1 - cnt0)
+  -- the one (1) is "experiment-number" 
+  report <- withConfig (defaultConfig {verbosity = Verbose} ) $
+                runAndAnalyseOne 1 ("ImageSize " ++ show image_size)
+                                   (whnfIO runIt)
 
-          lift $ putStrLn $ "COMPILATION_TIME: " ++ show (diffUTCTime compile_t1 compile_t0)
+  putStrLn $ show (reportName report)
+--  putStrLn $ show (reportMeasured report) 
+  putStrLn $ show (reportAnalysis report) 
     
-          lift $ putStrLn $ "BYTES_TO_DEVICE: " ++ show 0
-          lift $ putStrLn $ "BYTES_FROM_DEVICE: " ++ show (fromIntegral (s*s))
-          lift $ putStrLn $ "TRANSFER_TO_DEVICE: " ++ show (diffUTCTime transfer_done transfer_start)
-          lift $ putStrLn $ "TRANSFER_FROM_DEVICE: " ++ show (diffUTCTime t_end t1)
-
-          -- in this case computed
-          lift $ putStrLn $ "ELEMENTS_PROCESSED: " ++ show (fromIntegral (s*s))
-          lift $ putStrLn $ "NUMBER_OF_BLOCKS: "   ++ show (fromIntegral s)
-          lift $ putStrLn $ "ELEMENTS_PER_BLOCK: " ++ show s 
-          
+ -- defaultMainWith (defaultConfig {forceGC = True})
+ --      [bgroup ("ImageSize " ++ show image_size)
+ --       [ bench "Obsidian" $ whnfIO runIt ]
+ --      ]
