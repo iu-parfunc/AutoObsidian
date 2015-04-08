394cdc26cce9884c6fb9d67dc228cf7e45d5bd6b
Michael Vollmer <vollmerm@indiana.edu>
2015-04-08 17:29:16 -0400

    Changed to BitString

--- Applications/Mandel/FractalsMain.hs
+++ Applications/Mandel/FractalsMain.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Main where
 
@@ -16,7 +17,7 @@ import Obsidian.Run.CUDA.Exec
 import Control.Monad.State
 
 -- Autotuning framework 
--- import Auto.Score
+import Auto.Score
 import System.Random (mkStdGen, random)
 import GA (Entity(..), GAConfig(..), evolveVerbose)
 
@@ -33,40 +34,41 @@ import Control.Exception
 
 -- parameters
 -- threads   = 256
-blocks, imageSize, identity, count, maxNum :: Int
+blocks, imageSize, identity, count, maxNum, numBits :: Int
 blocks    = 64
 imageSize = 1024
 identity  = 256
 count     = 10
-maxNum    = 512
-
-type Number = Int
+maxNum    = 960
+numBits   = 10
 
 -- genetic algorithm
-instance Entity Number Double () () IO where
+instance Entity BitString Double Context () IO where
 
   -- Generate a random starting number between 0 and maxNum.
-  genRandom _ seed = return $ 1 + ((fst $ random $ mkStdGen seed) `mod` maxNum)
+  genRandom _ seed = return $ 
+    let n = 1 + ((fst $ random $ mkStdGen seed) `mod` maxNum)
+    in numToBitString n numBits
 
   -- Ideally we'd be working with a bit neighborhood here for the Int,
   -- rather than just having a few bits of arithmetic.
-  crossover _ _ seed e1 e2 = return $ Just $ case seed `mod` 3 of
-                                                  0 -> (e1+e2) `mod` maxNum
-                                                  1 -> (abs (e1-e2)) `mod` maxNum
-                                                  2 -> (e1+e2) `div` 2
-                                                  _ -> error "crossover: unknown case"
+  crossover _ _ seed e1 e2 = return $ Just $
+    let i = (fst $ random $ mkStdGen seed) `mod` numBits
+    in crossBitString e1 e2 i
 
   -- Dumb mutation function. Should be smarter, see above.
-  mutation _ _ seed e = return $ Just $ if seed `mod` 2 == 0
-                                        then e +(1 + seed `mod` 10)
-                                        else abs (e - (1 + seed `mod` 10))
+  mutation _ _ seed e = return $ Just $
+    let i = (fst $ random $ mkStdGen seed) `mod` numBits
+    in mutateBitString e i
 
   -- Return a number denoting a score. Lower is better.
-  score _ threads = do
-
+  score ctx e = do
+    
+    -- Should memoize this function with a mutable table!
+    
+    let threads = bitStringToNum e
+    
     putStrLn $ "Trying with threads = " ++ (show threads)
-
-    ctx <- initialise
 
     kern <- captureIO ("kernel" ++ show identity)
             (props ctx)
@@ -79,7 +81,7 @@ instance Entity Number Double () () IO where
               o <== (fromIntegral imageSize,kern)
               syncAll
               copyOut o
-    catch
+    if threads == 0 then return Nothing else catch
       ( do
            -- This could be criterion instead, but it was complicated
            t0   <- getCurrentTime
@@ -95,8 +97,8 @@ instance Entity Number Double () () IO where
 main = do
   let cfg = GAConfig 
             5 -- population size
-            100 -- archive size (best entities to keep track of)
-            5 -- maximum number of generations
+            10 -- archive size (best entities to keep track of)
+            10 -- maximum number of generations
             0.8 -- crossover rate (% of entities by crossover)
             0.2 -- mutation rate (% of entities by mutation)
             0.0 -- parameter for crossover (not used here)
@@ -107,10 +109,11 @@ main = do
   currTime <- getCurrentTime
   let timed = floor $ utctDayTime currTime :: Int
   let g = mkStdGen timed -- random generator
-
-  es <- evolveVerbose g cfg () ()
-  let e = snd $ P.head es :: Int
-  putStrLn $ "best entity: " ++ (show e)
+  ctx <- initialise 
+  es <- evolveVerbose g cfg () ctx
+  let e = snd $ P.head es :: BitString
+      v = bitStringToNum e
+  putStrLn $ "best entity: " ++ (show v)
   
   
 
--- src/Auto/Score.hs
+++ src/Auto/Score.hs
@@ -1,14 +1,54 @@
-{- 
-    Info: 
+module Auto.Score
+       (
+         BitString
+       , bitStringToNum
+       , mutateBitString
+       , crossBitString
+       , numToBitString
+       )
+       where
 
--} 
- 
-module Auto.Score where
-
+import Data.Array
+-- import Data.Array.Unboxed (UArray)
 
 type ScoreType = Double 
 
 class Score a where
   score :: a -> ScoreType
 
+type BitString = Array Int Bool
 
+-- Make a bitstring of size i.
+makeBitString :: Int -> BitString
+makeBitString i = array (0,i) [(j,False) | j <- [0..i]]
+
+-- Convert bitstring to number.
+bitStringToNum :: BitString -> Int
+bitStringToNum arr = go $ assocs arr
+  where go [] = 0
+        go ((_,False):xs) = go xs
+        go ((i,True):xs)  = (2^i) + (go xs)
+
+-- Flip bit at i in bitstring.
+mutateBitString :: BitString -> Int -> BitString
+mutateBitString arr i = arr // [(i,b)]
+  where b = not $ arr ! i
+
+-- Crossover bitstrings arr1 and arr2.
+crossBitString :: BitString -> BitString -> Int -> BitString
+crossBitString arr1 arr2 i = array s l
+  where s = bounds arr1
+        l = zipWith f (assocs arr1) (assocs arr2)
+        f (i1,v1) (_,v2)
+          | (i1 < i)  = (i1,v1)
+          | otherwise = (i1,v2)
+
+-- Convert int to list of 0s and 1s.
+numToBits :: Int -> [Int]
+numToBits 0 = []
+numToBits y = let (a,b) = quotRem y 2 in [b] ++ numToBits a
+
+-- Make bitstring of number i and length s.
+numToBitString :: Int -> Int -> BitString
+numToBitString i s = makeBitString s // str
+  where str = zip [0..] $ map (== 1) $ numToBits i
