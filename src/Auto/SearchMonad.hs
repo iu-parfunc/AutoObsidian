22212f4b6f24a71bd6c114dbba0e1d35012e7320
Joel Svensson <svenssonjoel@yahoo.se>
2015-04-30 13:31:09 -0400

    tiny bit of comments to SearchMonad.hs

--- src/Auto/SearchMonad.hs
+++ src/Auto/SearchMonad.hs
@@ -13,6 +13,19 @@
 
 -} 
 
+{-|
+Module      : Auto.SearchMonad
+Description : The SearchMonad class 
+Copyright   : (c) Bo Joel Svensson, 2015
+                  Michael Vollmer, 2015
+License     : GPL-3
+Maintainer  : 
+Stability   : experimental
+Portability : 
+
+This module contains the SearchMonad class and directly
+related functions. 
+-}
 module Auto.SearchMonad
        ( SearchMonad(..)
        , execSearch 
@@ -23,30 +36,30 @@ module Auto.SearchMonad
 import Control.Monad
 import Control.Monad.IO.Class
 
-
 import Auto.ResultLog 
 
 -- List of parameters and score
 -- TODO: Abstract this further
--- It allows only Int parameters and Double results...
+-- It allows only Int parameters
 
--- TODO: I think that runSearch
---       should return a full resultLog!
-
+-- | SearchMonad is parameterised over result (fitness) type.
 class ( Ord result
       , Monad (m result) ) => SearchMonad result m where
   
   type SearchConfig m
   type SearchAux    m 
-  
+
+  -- | Run a search to obtain a full resultlog together
+  --   with any Auxiliary data a specific instance decides to return.
   runSearch :: SearchConfig m
             -> m result (Maybe result)
             -> IO (SearchAux m, ResultLog result)
-            
+
+  -- | get one of the parameters being tuned over.
   getParam :: Int -> m result Int
 
 
-
+-- | similar to runSearch but does not return any Auxiliary data. 
 execSearch :: SearchMonad result m
            => SearchConfig m
            -> m result (Maybe result)
