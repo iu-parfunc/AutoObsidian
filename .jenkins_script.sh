d78f571107b519be643169e62cd403ecc471ea91
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-05-01 10:49:52 +1000

    jenkins: update script
    ...probably

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -12,7 +12,7 @@ fi
 $CABAL sandbox init
 
 OPTS="-j --ghc-option=-j3"
-$CABAL install $OPTS --run-tests ./Obsidian/ ./src/ ./Applications/Mandel ./Applications/Histogram
+$CABAL install $OPTS --run-tests . ./Obsidian/ ./Applications/Mandel ./Applications/Histogram
 
 # Test suite?  Benchmark Suite?
 # Currently this does not share code with .travis.yml, and the Travis version does more.
