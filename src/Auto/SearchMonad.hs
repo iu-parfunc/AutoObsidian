b6a5dabf6b166d00ae69d746f0492bfee35a16b0
Mike Vollmer <mike@recurial.com>
2015-04-30 23:23:16 -0400

    Fix cabal file

--- src/Auto/SearchMonad.hs
+++ src/Auto/SearchMonad.hs
@@ -1,53 +1,63 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{- LANGUAGE TypeSynonymInstances -}
-{- LANGUAGE FlexibleInstances -}
-{- LANGUAGE GeneralizedNewtypeDeriving -}
-{-# LANGUAGE TypeFamilies #-} 
-
-
-{- TODOs
-   # Further abstractions
-     Parameter types
-     result types (should just require an Ord instance) 
-
--} 
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeSynonymInstances       #-}
 
 {-|
 Module      : Auto.SearchMonad
-Description : The SearchMonad class 
+Description : The SearchMonad class
 Copyright   : (c) Bo Joel Svensson, 2015
                   Michael Vollmer, 2015
 License     : GPL-3
-Maintainer  : 
+Maintainer  :
 Stability   : experimental
-Portability : 
+Portability :
 
 This module contains the SearchMonad class and directly
-related functions. 
+related functions.
 -}
+
+{-
+
+TODOs for the SearchMonad abstraction:
+
+ * Further abstractions
+
+ * Parameter types
+
+ * result types (should just require an Ord instance)
+
+-}
+
 module Auto.SearchMonad
        ( SearchMonad(..)
-       , execSearch 
+       , execSearch
        , module Control.Monad  -- get access to forM_ etc
-       , module Control.Monad.IO.Class -- maybe just liftIO ? 
+       , module Control.Monad.IO.Class -- maybe just liftIO ?
        ) where
 
 import Control.Monad
 import Control.Monad.IO.Class
 
-import Auto.ResultLog 
+import Auto.ResultLog
 
 -- List of parameters and score
 -- TODO: Abstract this further
 -- It allows only Int parameters
 
 -- | SearchMonad is parameterised over result (fitness) type.
-class ( Ord result
-      , Monad (m result) ) => SearchMonad result m where
-  
-  type SearchConfig m
-  type SearchAux    m 
+--   Rather than a fitness function, a monadic action is performed
+--   to evaluate candidate solutions.
+--   Individual search strategies implement this class, and specify
+--   their own SearchConfig and SearchAux.
+class (Ord result, Monad (m result)) => SearchMonad result m where
+
+  type SearchConfig m -- ^ The configuration settings for a particular search.
+
+  type SearchAux    m -- ^ Values returned in addition to the result.
 
   -- | Run a search to obtain a full resultlog together
   --   with any Auxiliary data a specific instance decides to return.
@@ -55,15 +65,17 @@ class ( Ord result
             -> m result (Maybe result)
             -> IO (SearchAux m, ResultLog result)
 
-  -- | get one of the parameters being tuned over.
+  -- | Get one of the parameters being tuned. Should be called from
+  --   the evaluation action.
   getParam :: Int -> m result Int
 
 
--- | similar to runSearch but does not return any Auxiliary data. 
+-- | Similar to runSearch but discards the SearchAux data and just
+--   returns the result.
 execSearch :: SearchMonad result m
            => SearchConfig m
            -> m result (Maybe result)
            -> IO (ResultLog result)
 execSearch conf m = do
-  (_,rlog) <- runSearch conf m 
-  return rlog 
+  (_,rlog) <- runSearch conf m
+  return rlog
