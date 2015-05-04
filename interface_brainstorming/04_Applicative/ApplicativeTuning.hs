3817cced1a3298ca58459cf997a60168949636f9
Ryan Newton <rrnewton@gmail.com>
2015-05-03 22:42:43 -0400

    Parameterize the Tune applicative over an underlying monad

--- interface_brainstorming/04_Applicative/ApplicativeTuning.hs
+++ interface_brainstorming/04_Applicative/ApplicativeTuning.hs
@@ -18,7 +18,6 @@ module ApplicativeTuning
  ) where
 
 import Control.Applicative
-import Data.List
 import System.Random.MWC
 
 -- | We restrict tunable parameters to Int values for now.
@@ -27,36 +26,44 @@ type Params = [Int]
 type Score = Double
 
 -- | The datatype for (auto)tunable computations.
-data Tune a =
+data Tune m a =
    -- TODO: run needs to return "m a" and Tune should be parameterized over "m".
-     Tune { _run :: (Params -> a)
+     Tune { _run :: (Params -> m a)
             -- ^ Run a tunable computation
           , allParams :: [Range] }
 
-instance Show (Tune a) where
+instance Show (Tune m a) where
   show (Tune {allParams}) =
     "<Tunable computation with params "++show allParams++">"
 
-instance Functor Tune where
-  fmap f (Tune g l) = Tune (f . g) l
+instance Monad m => Functor (Tune m) where
+  fmap f (Tune g l) = Tune (\p -> do x <- g p; return (f x)) l
 
-instance Applicative Tune where
-  pure x = Tune (\[] -> x) []
+instance Monad m => Applicative (Tune m) where
+  pure x = Tune (\[] -> return x) []
   Tune f l1 <*> Tune g l2 =
     let len1 = length l1
     in Tune (\ls -> let (x,y) = splitAt len1 ls
-                    in f x (g y)
+                    in do f' <- f x
+                          v  <- g y
+                          return $ f' v
             )
             (l1 ++ l2)
 
+effort :: Int
+effort = 1000
+
 -- | Run a tunable computation and retrieve the best found tuning
 -- parameters and their score, returning them in addition to the result.
-tune :: Tune (a,Score) -> IO (a, Params, Score)
+--
+-- Note, if the underlying monad were something OTHER than IO, then
+-- the tuner would need to be able to run it, so as to extract scores.
+tune :: Tune IO (a,Score) -> IO (a, Params, Score)
 tune (Tune f ranges) =
   -- Currently this does a simple random search:
   withSystemRandom $ asGenIO $ \gen ->
    do strt <- go gen
-      loop gen strt (1000::Int)
+      loop gen strt effort
   where
   loop _gen best 0 = return best
   loop gen best@(_,_,bscr) n =
@@ -66,43 +73,46 @@ tune (Tune f ranges) =
           else loop gen best (n-1)
   go gen =
     do params <- mapM (`uniformR` gen) ranges
-       let (a,sc) = f params
+       (a,sc) <- f params
        return (a,params,sc)
 
 -- | A quick way to run with all the *minimum* parameter settings,
 -- i.e. without performinging any tuning.
-runMin :: Tune a -> a
+runMin :: Tune m a -> m a
 runMin (Tune f ls) = f (map fst ls)
 
 -- | Fetch an integer in the range
-getParam :: Range -> Tune Int
-getParam r = Tune (\[p] -> p) [r]
+getParam :: Monad m => Range -> Tune m Int
+getParam r = Tune (\[p] -> return p) [r]
 
 -- | Inclusive ranges
 type Range = (Int,Int)
 
 
-example1 :: Tune Int
+example1 :: Tune IO Int
 example1 =
   (-) <$> getParam (1,10) <*> getParam (0,3)
 
-example2 :: Tune Double
+example2 :: Tune IO Double
 example2 =
   (\x y -> fromIntegral (x+y)) <$> getParam (11,12) <*> getParam (13,14)
 
 -- Illustrate composability:
-example3 :: Tune ((Int,Double),Score)
+example3 :: Tune IO ((Int,Double),Score)
 example3 = (\x y -> ((x,y), fromIntegral x + y))
            <$> example1 <*> example2
 
 test :: IO ()
 test =
   do putStrLn "First, runMin:"
-     putStrLn $ "  example1: "++show (runMin example1)
-     putStrLn $ "  example2: "++show (runMin example2)
-     putStrLn $ "  example3: "++show (runMin example3)
+     x <- (runMin example1)
+     y <- (runMin example2)
+     z <- (runMin example3)
+     putStrLn $ "  example1: "++show x
+     putStrLn $ "  example2: "++show y
+     putStrLn $ "  example3: "++show z
 
-     putStrLn "Next, random search"
-     x <- tune example3
-     print x
+     putStr "Next, random search\n  "
+     a <- tune example3
+     print a
      return ()
