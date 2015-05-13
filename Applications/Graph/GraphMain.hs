419e3a3e5ac434cef81c775911e0342d14c213ea
Joel Svensson <svenssonjoel@yahoo.se>
2015-05-13 01:08:25 -0400

    fix

--- Applications/Graph/GraphMain.hs
+++ Applications/Graph/GraphMain.hs
@@ -204,7 +204,7 @@ prog2Param = do
 
   kernel_th <- getParam 0
 
-  let small_th = small_th `mod` 64
+  let small_th = small_th' `mod` 64
   
   liftIO $ putStrLn $ "Trying with kernel_th = " ++ show kernel_th ++ "\n" ++ 
                       "and small_th = " ++ show small_th 
